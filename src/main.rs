b1911fe43d3317c046b601938c3d420fd3660552
cxnkk <cenkoezkartal@gmail.com>
2026-01-04 10:57:35 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,20 @@ use std::io::{self, Write};
 use std::process::exit;
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
 
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
-        if command.trim() == "exit" {
-            exit(0)
+        let trimmed_cmd = command.trim();
+        let input: Vec<&str> = trimmed_cmd.splitn(2, " ").collect();
+        match input[0] {
+            "exit" => exit(0),
+            "echo" => println!("{}", input[1]),
+            _ => {
+                println!("{}: command not found", trimmed_cmd);
+            }
         }
-        println!("{}: command not found", command.trim());
     }
 }
